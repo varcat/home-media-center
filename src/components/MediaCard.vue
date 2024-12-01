c5a727b610ad8fd8eb047ec8b05c2bcbd9bf6a32
shunping <810100157@qq.com>
2024-12-01 22:37:04 +0800

    feat: 增加全屏功能

---
+++ src/components/MediaCard.vue
@@ -0,0 +1,62 @@
+<script setup>
+const props = defineProps({
+  coverImg: String,
+  title: String,
+  releaseDate: String,
+});
+</script>
+
+<template>
+  <div class="card">
+    <div class="posterBox flex justify-center items-start">
+      <div class="date">{{ releaseDate }}</div>
+      <img :src="coverImg" :alt="title" class="poster" />
+    </div>
+    <div class="info">
+      <div class="title">{{ title }}</div>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+.card {
+  height: 100%;
+  cursor: pointer;
+  contain: content;
+  transition: all 0.3s;
+  display: flex;
+  flex-direction: column;
+}
+
+.posterBox {
+  position: relative;
+}
+.poster {
+  display: block;
+  width: 100%;
+  aspect-ratio: 2 / 3;
+  object-fit: fill;
+  border-radius: 6px;
+}
+
+.info {
+  flex-grow: 1;
+  line-height: 1.2;
+  width: 100%;
+  padding: 2px 4px;
+}
+.title {
+  font-weight: bold;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+.date {
+  position: absolute;
+  left: 2px;
+  top: 2px;
+  text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.6);
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useRouter } from "vue-router";
 import { onMounted, ref } from "vue";
+import MediaCard from "../components/MediaCard.vue";
 
 const router = useRouter();
 
@@ -23,16 +24,13 @@ onMounted(() => {
 <template>
   <div class="menus">
     <div class="container">
-      <div v-for="(x, i) of menus" @click="toDetail(x)" class="subject" >
-        <div class="posterBox flex justify-center items-start">
-          <img :src="x.coverImg" :alt="x.title" class="poster" />
-          <div class="subjectInfo">
-            <div class="title">{{ x.title }}</div>
-            <span class="text-3">({{ x.releaseDate }})</span>
-          </div>
-          
-        </div>
-      </div>
+      <MediaCard
+        v-for="(x, i) of menus"
+        @click="toDetail(x)"
+        :cover-img="x.coverImg"
+        :title="x.title"
+        :releaseDate="x.releaseDate"
+      />
     </div>
   </div>
 </template>
@@ -56,51 +54,10 @@ onMounted(() => {
     grid-template-columns: repeat(3, 1fr);
   }
 }
-@media screen and (max-width: 320px) {
+@media screen and (max-width: 375px) {
   .container {
-    grid-template-columns: repeat(2, 1fr);
-  }
-}
-
-
-.posterBox {
-  width: 100%;
-  height: 100%;
-  position: relative;
-  font-size: 14px;
-}
-.poster {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-.subjectInfo {
-  height: 80px;
-  padding: 2px 4px;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  background: linear-gradient(
-    to right,
-    rgba(0, 0, 0, 0.2),
-    rgba(200, 200, 255, 0.2)
-  );
-  backdrop-filter: blur(4px);
-}
-.title {
-  font-weight: bold;
-}
-
-.subject {
-  height: 100%;
-  font-size: 14px;
-  cursor: pointer;
-  contain: content;
-  transition: all .3s;
-
-  &:hover {
-    transform: translateY(-3px);
+    grid-template-columns: repeat(3, 1fr);
+    font-size: 12px;
   }
 }
 </style>
